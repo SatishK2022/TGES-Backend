2a33d8fe4d9312582da6aa69992c772ea7788adc
SatishK2022 <sk7399052@gmail.com>
2024-08-24 12:09:55 +0530

    update db connection

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,21 @@
-import mysql from "mysql2/promise"
-import dotenv from "dotenv"
-dotenv.config()
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASS || "@Satish2004",
+    database: process.env.DB_NAME || "tges",
+    waitForConnections: true,
+    connectionLimit: 10, 
+    queueLimit: 0
+});
 
 const connectToDb = async () => {
-    const connection = await mysql.createConnection({
-        host: process.env.DB_HOST || "localhost",
-        user: process.env.DB_USER || "root",
-        password: process.env.DB_PASS || "@Satish2004",
-        database: process.env.DB_NAME || "tges"
-    })
+    const connection = await pool.getConnection();
     return connection;
-}
+};
 
-export default connectToDb
+export default connectToDb;
